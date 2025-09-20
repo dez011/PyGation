4586fdfbcb7e64ac54abe21a1c3733252167d639
miguelh <miguel.hernandez011@gmail.com>
2025-09-20 18:06:13 -0500

    cam: removed restart always from service

--- piSecureKit/cams/zerov1/main.py
+++ piSecureKit/cams/zerov1/main.py
@@ -57,20 +57,20 @@ picam2.configure(video_config)
 # LQoutput = FfmpegOutput("-f rtsp -rtsp_transport udp rtsp://myuser:mypass@localhost:8554/lqstream", audio=False)
 
 # HQoutput = FfmpegOutput("-f rtsp -rtsp_transport tcp rtsp://192.168.6.76:8554/hqstream", audio=False)
-# HQoutput = FfmpegOutput(f"-c:v copy -an -f rtsp -rtsp_transport tcp rtsp://{HUB}:8554/hqstream", audio=False)
+HQoutput = FfmpegOutput(f"-c:v copy -an -f rtsp -rtsp_transport tcp rtsp://{HUB}:8554/hqstream", audio=False)
 LQoutput = FfmpegOutput(f"-c:v copy -an -f rtsp -rtsp_transport tcp rtsp://{HUB}:8554/lqstream", audio=False)
 
 # LQoutput = FfmpegOutput("-f rtsp -rtsp_transport tcp rtsp://192.168.6.76:8554/lqstream", audio=False)
 
 # Encoder settings
-# encoder_HQ = H264Encoder(bitrate=4_000_000, repeat=True, iperiod=30)
+encoder_HQ = H264Encoder(bitrate=4_000_000, repeat=True, iperiod=30)
 encoder_LQ = H264Encoder(repeat=True, iperiod=30, framerate=frame_rate, enable_sps_framerate=True)
 
 try:
     print("trying to start camera streams")
     # picam2.start_recording(encoder_HQ, HQoutput, quality=Quality.LOW)
-    picam2.start_recording(encoder_LQ, LQoutput, quality=Quality.LOW)
-    # picam2.start_recording(encoder_LQ, LQoutput, quality=Quality.LOW, name="lores")
+    # picam2.start_recording(encoder_LQ, LQoutput, quality=Quality.LOW)
+    picam2.start_recording(encoder_LQ, LQoutput, quality=Quality.LOW, name="lores")
     print("Started camera streams")
     while True:
         time.sleep(5)
