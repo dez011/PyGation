bba228d798af421b398cc8d38c4538c53bba938f
miguelh <miguel.hernandez011@gmail.com>
2025-09-20 18:02:58 -0500

    cam: removed restart always from service

--- piSecureKit/cams/zerov1/main.py
+++ piSecureKit/cams/zerov1/main.py
@@ -64,12 +64,12 @@ LQoutput = FfmpegOutput(f"-c:v copy -an -f rtsp -rtsp_transport tcp rtsp://{HUB}
 
 # Encoder settings
 # encoder_HQ = H264Encoder(bitrate=4_000_000, repeat=True, iperiod=30)
-encoder_LQ = H264Encoder(repeat=True, intra_period=30, framerate=frame_rate, enable_sps_framerate=True)
+encoder_LQ = H264Encoder(repeat=True, iperiod=30, framerate=frame_rate, enable_sps_framerate=True)
 
 try:
     print("trying to start camera streams")
     # picam2.start_recording(encoder_HQ, HQoutput, quality=Quality.LOW)
-    picam2.start_recording(encoder_LQ, LQoutput)
+    picam2.start_recording(encoder_LQ, LQoutput, quality=Quality.LOW)
     # picam2.start_recording(encoder_LQ, LQoutput, quality=Quality.LOW, name="lores")
     print("Started camera streams")
     while True:
